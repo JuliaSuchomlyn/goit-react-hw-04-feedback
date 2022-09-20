d3880564b8beac7d57fd5215e55f335c7de900a6
Julia <juliasuchomlyn@gmail.com>
2022-09-20 18:34:19 +0300

    change hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,36 @@
-import React from 'react';
+import {useState} from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export class App extends React.Component {
-state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    };
+export const App = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
     
-    countTotalFeedback = () => {
-        return Object.values(this.state).reduce((total, item) => total + item, 0)
-    }
-
-    countPositiveFeedbackPercentage = (total, good) => {
-        const positivePercentages = Math.round((good / total) * 100)
-        return total === 0 ? 0 : positivePercentages ;
-
-    }
-    handleClick = (key) => {
-        this.setState(prevState => {
-            return {
-                [key]: prevState[key] + 1
+    const handleClickButton = buttonTitle => {
+        switch (buttonTitle) {
+            case 'good':
+                setGood(good => good + 1);
+                break;
+            
+            case 'neutral':
+                setNeutral(neutral => neutral + 1);
+                break;
+            
+            case 'bad':
+                setBad(bad => bad + 1);
+                break;
+            
+            default:
+                break;
             }
-        })
-    }
+        }
 
-    
+    const total = good + neutral + bad;
+    const positiveFeedback = Math.round((good / total) * 100);    
 
-    render() {
-        const { good, neutral, bad } = this.state;
-        const total = this.countTotalFeedback();
-        const positiveFeedback = this.countPositiveFeedbackPercentage(
-            total,
-            good
-        )
 
         return (            
           <div style={{
@@ -50,8 +44,8 @@ state = {
             <div className='container__element '>
                 <Section title="Please leave feedback">
                     <FeedbackOptions
-                        options={Object.keys(this.state)}
-                        clickFeedback={this.handleClick}                    
+                        options={Object.keys({good, neutral, bad})}
+                        clickFeedback={handleClickButton}                    
                     />
                 </Section>
 
@@ -88,4 +82,4 @@ state = {
   //   </div>
   // );
 
-};
+
